f795bd91ac0cc3022fc50a9c49628e19d28953b2
dengqi <dengqi@zhongan.com>
2020-01-08 22:04:36 +0800

    ss

--- src/main/java/com/kaka/controllar/JenkinsControllar.java
+++ src/main/java/com/kaka/controllar/JenkinsControllar.java
@@ -19,7 +19,7 @@ public class JenkinsControllar {
 
     @RequestMapping(value = "/api/operation",method = RequestMethod.GET)
     public String doOperation(@RequestParam String name) {
-        return jenkinsService.start(name);
-//        return jenkinsService.stop(name);
+//        return jenkinsService.start(name);
+        return jenkinsService.stop(name);
     }
 }
